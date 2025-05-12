e34032d9b283952bc6fd5b1e2f02c2f4ecc5df8e
CandeGioia <cgioia@biwares.com>
2025-05-12 00:43:51 -0300

    correct images

--- pages/3_Creditos.py
+++ pages/3_Creditos.py
@@ -6,20 +6,24 @@ st.set_page_config(
 )
 st.title('Créditos ✍️')
 
-# Create centered layout using columns
-left_pad, center_col, right_pad = st.columns([1, 3, 1])
+st.markdown('La integral elíptica fue descubierta por Niels Henrik Abel:')
 
+left_pad, center_col, right_pad = st.columns([1, 3, 1])
 with center_col:
     # Niels Henrik Abel section
-    st.markdown('La integral elíptica fue descubierta por Niels Henrik Abel:')
+   
     st.image('Niels_Henrik_Abel.jpg', width=300)  # Fixed width of 300px
     
     # Wilhelm Cauer section
-    st.markdown('La función aproximación de mínimo orden fue definida por Wilhelm Cauer:')
+
+st.markdown('La función aproximación de mínimo orden fue definida por Wilhelm Cauer:')
+left_pad, center_col, right_pad = st.columns([1, 3, 1])
+with center_col:
+
     st.image('photo_cauer.jpg', width=300)  # Same fixed width for consistency
     
-    # Credits section
-    st.markdown('La siguiente aplicación fue realizada por la cátedra Teoría de Circuitos 2 del ITBA:')
-    st.markdown("- Candela Gioia")
-    st.markdown("- Juan Francisco Sbruzzi")
-    st.markdown("- Javier Petrucci")
+# Credits section
+st.markdown('La siguiente aplicación fue realizada por la cátedra Teoría de Circuitos 2 del ITBA:')
+st.markdown("- Candela Gioia")
+st.markdown("- Juan Francisco Sbruzzi")
+st.markdown("- Javier Petrucci")

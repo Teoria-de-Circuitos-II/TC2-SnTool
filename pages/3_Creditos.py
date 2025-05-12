e0810b92d23c31f9db3e44fa008b2eea58035e1c
Javier Petrucci <javierpetrucci@gmail.com>
2025-05-12 00:39:15 -0300

    Cred Centrados

--- pages/3_Creditos.py
+++ pages/3_Creditos.py
@@ -2,22 +2,24 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Créditos",
-    page_icon= "✍️",
+    page_icon="✍️",
 )
 st.title('Créditos ✍️')
 
+# Create centered layout using columns
+left_pad, center_col, right_pad = st.columns([1, 3, 1])
 
-
-
-st.markdown('La integral elíptica fue descubierta por Niels Henrik Abel: ')
-st.image('Niels_Henrik_Abel.jpg')
-
-st.markdown('La función aproximación de mínimo orden fue definida por Wilhelm Cauers')
-st.image('photo_cauer.jpg')
-
-st.markdown('La siguiente aplicación fue realizada por la cátedra Teoría de Circuitos 2 del ITBA:')
-
-st.markdown("- Candela Gioia")
-st.markdown("- Juan Francisco Sbruzzi")
-st.markdown("- Javier Petrucci")
-            
+with center_col:
+    # Niels Henrik Abel section
+    st.markdown('La integral elíptica fue descubierta por Niels Henrik Abel:')
+    st.image('Niels_Henrik_Abel.jpg', width=300)  # Fixed width of 300px
+    
+    # Wilhelm Cauer section
+    st.markdown('La función aproximación de mínimo orden fue definida por Wilhelm Cauer:')
+    st.image('photo_cauer.jpg', width=300)  # Same fixed width for consistency
+    
+    # Credits section
+    st.markdown('La siguiente aplicación fue realizada por la cátedra Teoría de Circuitos 2 del ITBA:')
+    st.markdown("- Candela Gioia")
+    st.markdown("- Juan Francisco Sbruzzi")
+    st.markdown("- Javier Petrucci")
